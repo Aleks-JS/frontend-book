87a03d84312f90a9e30a20ed9a985432a77034ec
Aleksey Kresnikov <kresnikov@yandex.ru>
2020-07-20 10:56:46 +0300

    Added methods to the constructor for accessing data and for deleting data by key

--- coffeerun/scripts/datastore.js
+++ coffeerun/scripts/datastore.js
@@ -5,8 +5,24 @@
     this.data = {};
   }
 
+  // Прототип конструктора *en* Constructor prototype
   DataStore.prototype.add = function (key, val) {
     this.data[key] = val;
+  };
+
+  // Создаем метод для поиска значения по заданному ключу *en* Create a method for finding a value by a given key
+  DataStore.prototype.get = function (key) {
+    return this.data[key];
+  };
+
+  // Метод для поиска всех ключей и значений *en* Method to find all keys and values
+  DataStore.prototype.getAll = function () {
+    return this.data;
+  };
+
+  // Метод для удаления информации *en* Method for removing information
+  DataStore.prototype.remove = function (key) {
+    delete this.data[key];
   };
 
   App.DataStore = DataStore;
