9748cf61acdeffc040beb9383163d59df4ce3ca1
Aleksey Kresnikov <kresnikov@yandex.ru>
2020-07-21 16:43:57 +0300

    Configuring FormHandler Instances Using Selectors

--- coffeerun/scripts/formhandler.js
+++ coffeerun/scripts/formhandler.js
@@ -1,7 +1,15 @@
 (function (window) {
   const App = window.App || {};
   const $ = window.jQuery;
-  function FormHandler() {}
+  function FormHandler(selector) {
+    if (!selector) {
+      throw new Error('No selector provided');
+    }
+    this.$formElement = $(selector);
+    if (this.$formElement.length === 0) {
+      throw new Error(`Could not find element with selector: ${selector}`);
+    }
+  }
 
   App.FormHandler = FormHandler;
   window.App = App;
