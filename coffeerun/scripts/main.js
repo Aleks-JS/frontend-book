50f50a4ed975f1d292984d2d0ee181a10f1df317
Aleksey Kresnikov <kresnikov@yandex.ru>
2020-07-21 19:03:56 +0300

    Using a formHandler instance

--- coffeerun/scripts/main.js
+++ coffeerun/scripts/main.js
@@ -1,8 +1,14 @@
 (function (window) {
+  const FORM_SELECTOR = '[data-coffee-order="form"]';
   const { App } = window;
   const { Truck } = App;
   const { DataStore } = App;
-
+  const { FormHandler } = App;
   const myTruck = new Truck('ncc-1701', new DataStore());
+  const formHandler = new FormHandler(FORM_SELECTOR);
+
+  // Регистрация метода createOrdrer в качестве обработчика события
+  formHandler.addSubmitHandler(myTruck.createOrder.bind(myTruck));
+  console.log(formHandler);
   window.myTruck = myTruck;
 })(window);
